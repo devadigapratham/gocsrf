0c191c9d1ce1decd9da9efc9e30d5171064fb2ca
devadigapratham <devadigapratham8@gmail.com>
2024-06-17 00:00:09 +0530

    too much to fix, will do tom

--- server/middleware/myJwt/myJwt.go
+++ server/middleware/myJwt/myJwt.go
@@ -1,15 +1,55 @@
 package myJwt
 
+import (
+	"io/ioutil"
+	"time"
+
+	"github.com/devadigapratham/gocsrf/db/models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
 const (
 	privKeyPath = "keys/app.rsa"
 	pubKeyPath  = "keys/app.rsa.pub"
 )
 
 func InitJWT() error {
+	signBytes, err := ioutil.ReadFile(privKeyPath)
+	if err != nil {
+		return err
+	}
 
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	if err != nil {
+		return err
+	}
+
+	verifyBytes, err := ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		return err
+	}
+
+	verifyKey, err := jwt.ParseRSAPrivateKeyFromPEM(verifyBytes)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func CreateNewTokens() {
+func CreateNewTokens(uuid string, role string) (authTokenString, refreshToken string, csrfSecret string, err error) {
+	//generate csrf secret
+	csrfSecret, err = models.GenerateCSRFSecret()
+	if err != nil {
+		return
+	}
+	//generate refresh token
+	refreshTokenString, err = createRefreshTokenString(uuid, role, csrfSecret)
+	//generating auth token
+	authTokenString, err = createAuthTokenString(uuid, role, csrfSecret)
+	if err != nil {
+		return
+	}
+	return
 
 }
 
@@ -17,7 +57,8 @@ func CreateAndRefreshTokens() {
 
 }
 
-func createAuthTokenString() {
+func createAuthTokenString(uuid string, role string, csrfSecret string) (authTokenString, err error) {
+	authTokenExp := time.Now().Add(models.AuthTokenValidTime).Unix()
 
 }
 
